4270fe2e29dbfe713df3924e8d5dd0bbb94e4e06
Garrett Bartley <wgbartley@gmail.com>
2015-10-09 13:42:13 -0400

    Changed "particle-stats-all" to "statsd-graphite"

--- irun.sh
+++ irun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo docker run -t -i \
-	--name=particle-stats-all \
-	-h particle-stats-all \
+	--name=statsd-graphite \
+	-h statsd-graphite \
 	-v $(pwd)/data:/storage \
 	-p 80:80 \
 	-p 2003:2003 \
 	-p 8125:8125/udp \
 	-p 8126:8126 \
-	wgbartley/particle-stats-all /bin/bash
+	wgbartley/statsd-graphite /bin/bash
